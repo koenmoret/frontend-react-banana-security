ed54e24ea3ff44f8f2d48d9709ea85977582054c
koenmoret <koen@moret.info>
2024-04-01 19:39:37 +0200

    feature/fields

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,19 +1,63 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
+import {AuthContext} from "../context/AuthContext";
 
 function SignUp() {
-  return (
-    <>
-      <h1>Registreren</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque consectetur, dolore eaque eligendi
-        harum, numquam, placeat quisquam repellat rerum suscipit ullam vitae. A ab ad assumenda, consequuntur deserunt
-        doloremque ea eveniet facere fuga illum in numquam quia reiciendis rem sequi tenetur veniam?</p>
-      <form>
-        <p>*Invoervelden*</p>
-      </form>
-      <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
-    </>
-  );
+
+    const {signup, username, setUsername, email, setEmail, setPassword, password} = useContext(AuthContext);
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        signup(username, email, password);
+    }
+
+    return (
+        <>
+            <h1>Registreren</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id
+                molestias qui quo unde?</p>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Gebruikersnaam:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Wachtwoord:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Aanmelden</button>
+            </form>
+
+            <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
+        </>
+    );
 }
 
 export default SignUp;
